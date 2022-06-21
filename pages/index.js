4c6067ce3e4a5e1fc7a71665c4c020c03d465767
evey201 <evesho.alabi@gmail.com>
2022-06-21 13:31:13 +0300

    removed unused vars

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
+// import Head from 'next/head'
+// import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 import {HeaderWrapper} from '../styles/Home'
 
