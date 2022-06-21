18355c662402855a47a64857b0e575377105b880
evey201 <evesho.alabi@gmail.com>
2022-06-21 13:30:18 +0300

    added rules

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
@@ -5,7 +6,7 @@ import {HeaderWrapper} from '../styles/Home'
 
 export default function Home() {
   return (
-    <div className=''>
+    <div className="">
       <HeaderWrapper>
         dhsfuidhfiu
       </HeaderWrapper>
